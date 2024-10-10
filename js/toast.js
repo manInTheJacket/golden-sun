2951a733cf505d2ed0a96d92a4e089dbc7a38066
Ilyas <fortwink88@gmail.com>
2024-10-10 20:37:48 +0500

    upgraded toast

--- js/toast.js
+++ js/toast.js
@@ -1,13 +1,36 @@
+const forms = document.getElementById('classForm');
+
+// Add an event listener to the submit button
+document.querySelector('.button').onclick = function(event) {
+    event.preventDefault(); // Prevent default form submission
+
+    // Check form validity
+    if (!forms.checkValidity()) {
+        // Show validation error message
+        displayToastNotification(
+            "Please fill in all required fields.", 
+            "fa fa-exclamation-triangle", 
+            "#f39c12", 
+            "slide-in-fade-out"
+        );
+    } else {
+        // Show success message
+        displayToastNotification(
+            "Registration successful!", 
+            "fa fa-check", 
+            "#27ae60", 
+            "slide-in-slide-out"
+        );
+
+        // Redirect to payment.html after a delay
+        setTimeout(function() {
+            window.location.href = "payment.html";
+        }, 2000); // 2-second delay before redirect
+    }
+};
+
+
 var toastCounter = 1;
-
-$('button').on('click', function() {
-	var msg = $(this).attr('msg');
-	var icon = $(this).attr('icon');
-	var icon_color = $(this).attr('icon-color');
-	var animation = $(this).attr('animation');
-
-	displayToastNotification(msg, icon, icon_color, animation);
-});
 
 function displayToastNotification( msg, icon, icon_color, animation ) {
 	var class_name = 'toast-'+toastCounter;
@@ -20,8 +43,6 @@ function displayToastNotification( msg, icon, icon_color, animation ) {
 	new_node.removeClass('hide-toast').addClass(animation);
 	setTimeout(function() {
 		new_node.remove();
-        window.location.href = 'payment.html'; 
 	}, 3800);
-    
 	toastCounter++;
 }
